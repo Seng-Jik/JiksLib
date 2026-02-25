ccf24b27486b34d016051ddfe4e4b6bbb5ce4af5
许兴逸 <853974536@qq.com>
2026-02-26 00:13:04 +0800

    EventBus性能测试套件

---
+++ JiksLib.PerformanceTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace JiksLib.PerformanceTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 使用 BenchmarkSwitcher 允许通过命令行参数选择要运行的基准测试
+            var switcher = new BenchmarkSwitcher(new[]
+            {
+                typeof(EventBusBenchmarks),
+                typeof(ValueEventBusBenchmarks),
+                typeof(EventBusComparisonBenchmarks)
+            });
+
+            switcher.Run(args);
+        }
+    }
+}
