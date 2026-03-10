f44c522ed43a1b01fa3c7351b20c6e66fab0a910
许兴逸 <853974536@qq.com>
2026-03-10 23:38:34 +0800

    更新设计

--- JiksLib.Core/UI/RedPointSolver.cs
+++ JiksLib.Core/UI/RedPointSolver.cs
@@ -3,45 +3,77 @@ using System.Collections.Generic;
 
 namespace JiksLib.UI
 {
-    // todo: 暴露出RedPoint类
     /// <summary>
     /// 红点解算器
     /// 红点构成一个有向无环图
+    /// 红点有两个键，键A和键B联合起来可以索引到一个红点
+    /// 键A表示静态红点的部分，键B表示动态红点的部分
     /// </summary>
-    /// <typeparam name="TRedPointKey">红点键</typeparam>
+    /// <typeparam name="TKeyA">一级红点键</typeparam>
     /// <typeparam name="TUserData">红点附带的用户信息类型</typeparam>
-    public sealed class RedPointSolver<TRedPointKey, TUserData>
-        where TRedPointKey : notnull
+    public sealed class RedPointSolver<TKeyA, TUserData>
+        where TKeyA : notnull
     {
         /// <summary>
         /// 红点监听器
         /// </summary>
-        /// <param name="redPointKey">红点键</param>
-        /// <param name="userData">红点附带的用户信息</param>
-        /// <param name="redPointNumber">红点的值</param>
-        public delegate void RedPointListener(
-            TRedPointKey redPointKey,
+        /// <typeparam name="TKeyB">二级红点键</typeparam>
+        public delegate void RedPointListener<TKeyB>(
+            TKeyA keyA,
+            TKeyB keyB,
             TUserData userData,
-            int? redPointNumber);
+            int? redPointNumber)
+            where TKeyB : notnull;
+
+        public interface IRedPointChecker
+        {
+            /// <summary>
+            /// 检查所有动态红点
+            /// </summary>
+            /// <param name="redPointNumberSum">所有动态红点的值之和</param>
+            /// <returns>是否有动态红点</returns>
+            bool CheckAll(out int redPointNumberSum);
+        }
 
         /// <summary>
         /// 红点检查器
         /// </summary>
+        public interface IRedPointChecker<TKeyB> : IRedPointChecker
+            where TKeyB : notnull
+        {
+            /// <summary>
+            /// 检查单个动态红点
+            /// </summary>
+            /// <param name="keyB">二级红点键</param>
+            /// <param name="redPointNumber">红点值</param>
+            /// <param name="userData">红点附带的用户信息</param>
+            /// <returns>该红点是否存在</returns>
+            bool Check(
+                TKeyB keyB,
+                out int redPointNumber,
+                out TUserData userData);
+        }
+
+        /// <summary>
+        /// 简单的红点检查器
+        /// </summary>
+        /// <typeparam name="TKeyB">二级红点键</typeparam>
         /// <param name="key">红点键</param>
         /// <param name="userData">红点附带的用户信息</param>
         /// <param name="number">输出的红点的值</param>
-        public delegate bool RedPointChecker(
-            TRedPointKey key,
-            TUserData userData,
+        public delegate bool SimpleRedPointChecker(
+            TKeyA keyA,
             out int number);
 
         /// <summary>
         /// 检查红点是否存在及红点值
         /// </summary>
-        public bool Check(
-            TRedPointKey key,
+        public bool Check<TKeyB>(
+            TKeyA keyA,
+            TKeyB keyB,
             out int number,
             out TUserData userData)
+            where TKeyB : notnull
         {
             throw new NotImplementedException();
         }
@@ -50,38 +82,11 @@ namespace JiksLib.UI
         /// 检查红点是否存在，不计算红点值
         /// 不需要红点值时使用该函数，性能更好
         /// </summary>
-        public bool Check(
-            TRedPointKey key,
+        public bool Check<TKeyB>(
+            TKeyA keyA,
+            TKeyB keyB,
             out TUserData userData)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// 添加红点监听器
-        /// 当红点变更时触发
-        /// </summary>
-        public void AddListener(
-            TRedPointKey key,
-            RedPointListener listener)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// 移除红点监听器
-        /// </summary>
-        public void RemoveListener(
-            TRedPointKey key,
-            RedPointListener listener)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// 检查指定红点的值，并触发红点监听器
-        /// </summary>
-        public void UpdateRedPoint(TRedPointKey key)
+            where TKeyB : notnull
         {
             throw new NotImplementedException();
         }
@@ -92,117 +97,49 @@ namespace JiksLib.UI
         public sealed class Builder
         {
             /// <summary>
-            /// 添加一个红点，附带一个检查器
+            /// 添加一个红点检查器
+            /// 通过 KeyA 来索引到该检查器
+            /// 通过 KeyB 来索引到该检查器中的红点
             /// </summary>
-            public void AddRedPoint(
-                TRedPointKey key,
-                TUserData userData,
-                RedPointChecker checker)
-            {
-                redPointLayout.Add(key, (userData, checker, null!));
-            }
-
-            /// <summary>
-            /// 添加一个红点，绑定一个或多个子红点
-            /// 当一个子红点有值时，该红点有值
-            /// 该红点的值为所有子红点值之和
-            /// </summary>
-            public void AddRedPoint(
-                TRedPointKey key,
-                TUserData userData,
-                params TRedPointKey[] children)
-            {
-                redPointLayout.Add(key, (userData, null, children));
-            }
-
-            /// <summary>
-            /// 构建解算器
-            /// </summary>
-            public RedPointSolver<TRedPointKey, TUserData> Build()
+            public void AddRedPointChecker<TKeyB>(
+                TKeyA keyA,
+                IRedPointChecker<TKeyB> checker)
+                where TKeyB : notnull
             {
                 throw new NotImplementedException();
             }
 
-            readonly Dictionary<TRedPointKey, (TUserData, RedPointChecker?, TRedPointKey[])>
-                redPointLayout = new();
-        }
-
-        private abstract class RedPoint
-        {
-            internal readonly TUserData UserData;
-            internal readonly TRedPointKey Key;
-            internal readonly List<RedPoint> Parents;
-
-            internal RedPoint(TRedPointKey key, TUserData userData)
+            /// <summary>
+            /// 添加一个简单红点检查器
+            /// KeyB 会被设置为 UnitType
+            /// </summary>
+            public void AddStaticRedPoint(
+                TKeyA key,
+                SimpleRedPointChecker checker,
+                TUserData userData)
             {
-                Key = key;
-                UserData = userData;
-                Parents = new();
+                throw new NotImplementedException();
             }
 
-            internal abstract bool Check(out int number);
-            internal abstract bool Check();
-        }
-
-        private sealed class RedPointLeaf : RedPoint
-        {
-            readonly RedPointChecker checker;
-
-            internal RedPointLeaf(
-                TRedPointKey key,
+            /// <summary>
+            /// 添加一个组合红点，TKeyA 在 children 中的所有红点设置为子红点
+            /// 当一个子红点有值时，该红点有值
+            /// 该红点的值为所有子红点值之和
+            /// </summary>
+            public void AddRedPointComposite(
+                TKeyA key,
                 TUserData userData,
-                RedPointChecker checker)
-                : base(key, userData)
+                params TKeyA[] children)
             {
-                this.checker = checker;
+                throw new NotImplementedException();
             }
 
-            internal override bool Check(out int number)
+            /// <summary>
+            /// 构建红点解算器
+            /// </summary>
+            public RedPointSolver<TKeyA, TUserData> Build()
             {
-                if (checker(Key, UserData, out number))
-                    return true;
-
-                number = 0;
-                return false;
-            }
-
-            internal override bool Check() =>
-                checker(Key, UserData, out _);
-        }
-
-        private sealed class RedPointComposite : RedPoint
-        {
-            internal readonly List<RedPoint> Children = new();
-
-            internal RedPointComposite(
-                TRedPointKey key,
-                TUserData userData) : base(key, userData)
-            { }
-
-            internal override bool Check(out int number)
-            {
-                number = 0;
-                bool result = false;
-
-                foreach (var child in Children)
-                {
-                    if (child.Check(out var childNumber))
-                    {
-                        number += childNumber;
-                        result = true;
-                    }
-                }
-
-                return result;
-            }
-
-            internal override bool Check()
-            {
-                foreach (var child in Children)
-                    if (child.Check())
-                        return true;
-
-                return false;
+                throw new NotImplementedException();
             }
         }
     }
