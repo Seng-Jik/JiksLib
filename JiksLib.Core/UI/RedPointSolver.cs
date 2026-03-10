34602adc1b23db527dc5e5a56056031f92403436
许兴逸 <853974536@qq.com>
2026-03-11 00:17:28 +0800

    Bug fixed.

--- JiksLib.Core/UI/RedPointSolver.cs
+++ JiksLib.Core/UI/RedPointSolver.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 
 namespace JiksLib.UI
 {
@@ -7,7 +6,7 @@ namespace JiksLib.UI
     /// 红点解算器
     /// 红点构成一个有向无环图
     /// 红点有两个键，键A和键B联合起来可以索引到一个红点
-    /// 键A表示静态红点的部分，键B表示动态红点的部分
+    /// 键A与检查器绑定，键B由检查器解释
     /// </summary>
     /// <typeparam name="TKeyA">一级红点键</typeparam>
     /// <typeparam name="TUserData">红点附带的用户信息类型</typeparam>
@@ -15,16 +14,9 @@ namespace JiksLib.UI
         where TKeyA : notnull
     {
         /// <summary>
-        /// 红点监听器
+        /// 红点检查器
+        /// 不要直接使用该类型，要使用 IRedPointChecker<TKeyB>
         /// </summary>
-        /// <typeparam name="TKeyB">二级红点键</typeparam>
-        public delegate void RedPointListener<TKeyB>(
-            TKeyA keyA,
-            TKeyB keyB,
-            TUserData userData,
-            int? redPointNumber)
-            where TKeyB : notnull;
-
         public interface IRedPointChecker
         {
             /// <summary>
@@ -32,7 +24,12 @@ namespace JiksLib.UI
             /// </summary>
             /// <param name="redPointNumberSum">所有动态红点的值之和</param>
             /// <returns>是否有动态红点</returns>
-            bool CheckAll(out int redPointNumberSum);
+            bool CheckFamily(out int redPointNumberSum);
+
+            /// <summary>
+            /// 是否存在任一红点
+            /// </summary>
+            bool CheckFamily();
         }
 
         /// <summary>
@@ -55,11 +52,10 @@ namespace JiksLib.UI
         }
 
         /// <summary>
-        /// 简单的红点检查器
+        /// 简单的红点检查器，只绑定一个红点
+        /// 该检查器的二级红点键始终为 new UnitType()
         /// </summary>
-        /// <typeparam name="TKeyB">二级红点键</typeparam>
-        /// <param name="key">红点键</param>
-        /// <param name="userData">红点附带的用户信息</param>
+        /// <param name="keyA">一级红点键</param>
         /// <param name="number">输出的红点的值</param>
         public delegate bool SimpleRedPointChecker(
             TKeyA keyA,
@@ -92,6 +88,83 @@ namespace JiksLib.UI
         }
 
         /// <summary>
+        /// 检查 KeyA 为某一值的所有红点
+        /// </summary>
+        public bool CheckFamily(TKeyA keyA, out int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 检查是否存在 KeyA 为某一值的红点
+        /// </summary>
+        public bool CheckFamily(TKeyA keyA)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 红点监听器
+        /// </summary>
+        /// <typeparam name="TKeyB">二级红点键</typeparam>
+        public delegate void RedPointListener<TKeyB>(
+            TKeyA keyA,
+            TKeyB keyB,
+            TUserData userData,
+            int? redPointNumber)
+            where TKeyB : notnull;
+
+        /// <summary>
+        /// 为键为 (keyA, keyB) 的红点添加监听器
+        /// 只能用 RemoveListener 删除
+        /// </summary>
+        public void AddListener<TKeyB>(
+            TKeyA keyA,
+            TKeyB keyB,
+            RedPointListener<TKeyB> listener)
+            where TKeyB : notnull
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 为键为 (keyA, keyB) 的红点删除监听器
+        /// 只能删除用 AddListener 添加的监听器
+        /// </summary>
+        public void RemoveListener<TKeyB>(
+            TKeyA keyA,
+            TKeyB keyB,
+            RedPointListener<TKeyB> listener)
+            where TKeyB : notnull
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 所有一级红点键为 keyA 的红点添加监听器
+        /// 只能用 RemoveListenerForFamily 删除
+        /// </summary>
+        public void AddListenerForFamily<TKeyB>(
+            TKeyA keyA,
+            RedPointListener<TKeyB> listener)
+            where TKeyB : notnull
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 为所有一级红点键为 keyA 的红点删除监听器
+        /// 只能用 AddListenerForFamily 添加的监听器
+        /// </summary>
+        public void RemoveListenerForFamily<TKeyB>(
+            TKeyA keyA,
+            RedPointListener<TKeyB> listener)
+            where TKeyB : notnull
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
         /// 红点解算器构造器
         /// </summary>
         public sealed class Builder
@@ -101,7 +174,7 @@ namespace JiksLib.UI
             /// 通过 KeyA 来索引到该检查器
             /// 通过 KeyB 来索引到该检查器中的红点
             /// </summary>
-            public void AddRedPointChecker<TKeyB>(
+            public void AddFamily<TKeyB>(
                 TKeyA keyA,
                 IRedPointChecker<TKeyB> checker)
                 where TKeyB : notnull
@@ -113,7 +186,7 @@ namespace JiksLib.UI
             /// 添加一个简单红点检查器
             /// KeyB 会被设置为 UnitType
             /// </summary>
-            public void AddStaticRedPoint(
+            public void AddSimple(
                 TKeyA key,
                 SimpleRedPointChecker checker,
                 TUserData userData)
@@ -122,11 +195,10 @@ namespace JiksLib.UI
             }
 
             /// <summary>
-            /// 添加一个组合红点，TKeyA 在 children 中的所有红点设置为子红点
+            /// 添加一个组合红点，组合红点的值为其子检查器下所有红点（包括所有KeyB）的值之和
             /// 当一个子红点有值时，该红点有值
-            /// 该红点的值为所有子红点值之和
             /// </summary>
-            public void AddRedPointComposite(
+            public void AddComposite(
                 TKeyA key,
                 TUserData userData,
                 params TKeyA[] children)
