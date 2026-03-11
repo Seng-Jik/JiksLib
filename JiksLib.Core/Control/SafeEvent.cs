f3ee3088f8afd268fc1b5251dd3453f7f7cc47f0
许兴逸 <853974536@qq.com>
2026-03-11 22:28:41 +0800

    添加文档

--- JiksLib.Core/Control/SafeEvent.cs
+++ JiksLib.Core/Control/SafeEvent.cs
@@ -4,15 +4,35 @@ using System.Runtime.CompilerServices;
 
 namespace JiksLib.Control
 {
+    /// <summary>
+    /// 安全事件处理器委托
+    /// </summary>
+    /// <typeparam name="TEvent">事件类型</typeparam>
+    /// <param name="event">事件对象</param>
     public delegate void EventHandler<TEvent>(TEvent @event);
 
+    /// <summary>
+    /// 安全事件包装器
+    /// 提供线程安全的事件发布/订阅机制，支持重入保护和异常安全
+    /// 仅支持在主线程上使用
+    /// </summary>
+    /// <typeparam name="TEvent">事件类型</typeparam>
     public sealed class SafeEvent<TEvent>
     {
+        /// <summary>
+        /// 构造安全事件包装器
+        /// </summary>
+        /// <param name="publisher">输出的事件发布器</param>
         public SafeEvent(out Publisher publisher)
         {
             publisher = new(this);
         }
 
+        /// <summary>
+        /// 添加事件监听器
+        /// 在事件处理过程中添加的监听器会延迟到处理完成后生效
+        /// </summary>
+        /// <param name="listener">事件监听器</param>
         public void AddListener(EventHandler<TEvent> listener)
         {
             if (invoking)
@@ -27,6 +47,11 @@ namespace JiksLib.Control
             }
         }
 
+        /// <summary>
+        /// 移除事件监听器
+        /// 在事件处理过程中移除的监听器会延迟到处理完成后生效
+        /// </summary>
+        /// <param name="listener">要移除的事件监听器</param>
         public void RemoveListener(EventHandler<TEvent> listener)
         {
             if (invoking)
@@ -61,8 +86,18 @@ namespace JiksLib.Control
             }
         }
 
+        /// <summary>
+        /// 事件发布器
+        /// 用于安全地触发事件
+        /// </summary>
         public readonly struct Publisher
         {
+            /// <summary>
+            /// 安全触发事件
+            /// 触发所有已注册的监听器，收集异常并防止递归调用
+            /// </summary>
+            /// <param name="event">事件对象</param>
+            /// <param name="exceptionsOutput">异常输出列表，监听器产生的异常将被收集到此列表中，为null则静默忽略异常</param>
             public readonly void SafeInvoke(
                 TEvent @event,
                 IList<Exception>? exceptionsOutput)
